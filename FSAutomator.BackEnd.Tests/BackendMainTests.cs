7162f6fac84e2bae84c757fff63b12359deb7dd7
albertfrutos <albertfg89gmail.com>
2023-04-12 01:00:04 +0200

    Added backend tests

--- FSAutomator.BackEnd.Tests/BackendMainTests.cs
+++ FSAutomator.BackEnd.Tests/BackendMainTests.cs
@@ -11,6 +11,7 @@ using Moq;
 using Newtonsoft.Json.Linq;
 using System.Collections.ObjectModel;
 using System.Reflection;
+using static FSAutomator.Backend.Entities.InternalMessage;
 
 namespace FSAutomator.Backend.Utilities.Tests
 {
@@ -19,17 +20,20 @@ namespace FSAutomator.Backend.Utilities.Tests
     {
         BackendMain backend;
         GeneralStatus status;
+        string currentDir;
 
         [TestInitialize()]
         public void TestInitialize()
         {
             backend = new BackendMain();
             status = GeneralStatus.GetInstance;
+            currentDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
         }
 
         [TestMethod]
         public void ValidateActions_ActionsWithNoIssuesAreUsed_ReturnsNoIssues()
         {
+            //Arrange
             backend.automator.ActionList.Add(
                 new FSAutomatorAction()
                 {
@@ -39,8 +43,10 @@ namespace FSAutomator.Backend.Utilities.Tests
                 }
             );
 
+            //Act
             var isActionValidated = ActionJSONValidator.ValidateActions(backend.automator.ActionList.ToArray());
 
+            //Assert
             status.ValidationIssues.Should().BeNull();
             backend.automator.ActionList[0].IsValidated.Should().BeTrue();
         }
@@ -48,6 +54,7 @@ namespace FSAutomator.Backend.Utilities.Tests
         [TestMethod]
         public void ValidateActions_ActionsWithOneIssueIsUsed_ReturnsOneIssue()
         {
+            //Arrange
             backend.automator.ActionList.Add(
                 new FSAutomatorAction()
                 {
@@ -57,10 +64,50 @@ namespace FSAutomator.Backend.Utilities.Tests
                 }
             );
 
+            //Act
             var isActionValidated = ActionJSONValidator.ValidateActions(backend.automator.ActionList.ToArray());
 
+            //Assert
             isActionValidated.Count.Should().Be(1);
+            isActionValidated[0].Should().Be("GetVariable [0]: Variable UNEXISTING VARIABLE does not exist.");
             backend.automator.ActionList[0].IsValidated.Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void SaveAutomation_FileNameIsEmpty_ReturnsError()
+        {
+            //Arrange
+            const string fileName = "";
+
+            //Act
+            var result = backend.SaveAutomation(null, fileName);
+
+            //Assert
+            result.Message.Should().Be("Please enter an automation name.");
+            result.Type.Should().Be(MsgType.Info);
+        }
+        
+        [TestMethod]
+        public void SaveAutomation_AutomationIsDLLAutomation_ReturnsError()
+        {
+            //Arrange
+            const string fileName = "testFileName.json";
+            const AutomationFile automationFile = new AutomationFile()
+            {
+                BasePath = "C:\\Users\\Albert\\Source\\Repos\\albertfrutos\\FSAutomator\\FSAutomator.UI\\bin\\Debug\\net6.0-windows\\Automations",
+                FileName = "ExternalAutomationExample.dll",
+                FilePath = "Automations\\ExternalAutomationExample.dll",
+                IsPackage = false,
+                PackageName = "",
+                VisibleName = "ExternalAutomationExample [.dll]"
+            };
+
+            //Act
+            var result = backend.SaveAutomation(null, fileName);
+
+            //Assert
+            result.Message.Should().Be("Please enter an automation name.");
+            result.Type.Should().Be(MsgType.Info);
         }
     }
 }
